a87d6eb904e7b88078c19d49ab922b80b71990bf
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-03-24 12:47:41 +0100

    commit inicial

---
+++ src/proyectofinal/Almacen.java
@@ -0,0 +1,19 @@
+package proyectofinal;
+public class Almacen {
+    public String Contenido;
+    public String Titulo;
+    public String Detalle;
+    public String Genero;    
+    public Almacen()
+    {}
+    public Almacen( String C, String T, String D, String G)
+    {
+        this.Contenido=C;
+        this.Titulo=T;
+        this.Detalle=D;
+        this.Genero=G;
+    }
+}
+
+
+
